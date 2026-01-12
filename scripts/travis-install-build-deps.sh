30fdb85790dd6f6c943a245983d9cfbd12dab066
Steffen Moeller <moeller@debian.org>
2026-01-12 00:50:34 +0000

    Move all task from debian/configure into normal debian/* files
    Using the 'nodoc' Debian package profile and build option to
    control the creation of documentation packages.
    
    Updated minmum debhelper compat level from 9 to 10, matching the level
    in Debian Buster.  Drop code to set higher compat level on newer versions
    of Debian.
    
    Uses versioned alternative depends to control the readline and
    texlive-xetex build dependency.
    
    Updated all build scripts and documentation to reflect that the
    debian/configure script is obsolete.

--- scripts/travis-install-build-deps.sh
+++ scripts/travis-install-build-deps.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -ex
 sudo apt-get update -qq
 sudo apt-get install -y devscripts equivs build-essential --no-install-recommends
-debian/configure
+sudo apt-get remove -f libreadline6-dev || true
+sudo apt-get remove -f libreadline-dev || true
+#
 # This gives an error that is hidden from us
 #mk-build-deps -i -r -s sudo -t 'apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --no-install-suggests'
 if ! sudo apt-get -y build-dep . ; then
